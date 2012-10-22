432a89fc57f3efab67cb9baac46a5d0db49ae5d4
zhangjinqiang <jasonzha@gmail.com>
2012-10-22 22:51:34 +1100

    multple circles moving

--- apps/circles_moving_in_container/TwoDRectangleBox.cpp
+++ apps/circles_moving_in_container/TwoDRectangleBox.cpp
@@ -1,65 +1,89 @@
 #include "TwoDRectangleBox.h"
 #include <QtOpenGL/QGLWidget>//find a better header to include opengl headers
 #include <cmath>
+#include <stdlib.h>
 
 TwoDRectangleBox::TwoDRectangleBox(double width, double length)
 :  m_width(width),
    m_length(length),
-   m_x(width/2),
-   m_y(length/2),
-   m_radius(std::min(width, length)/20),
-   m_x_speed(18),
-   m_y_speed(25)
+   m_circles()
 {
+    initialize();
 }
 
 TwoDRectangleBox::~TwoDRectangleBox()
 {
 }
 
-void TwoDRectangleBox::initialize()
-{
-
-}
-
 void TwoDRectangleBox::update(double deltaT)
 {
-    m_x += deltaT * m_x_speed;
-    m_y += deltaT * m_y_speed;
+    for (unsigned int i = 0; i < m_circles.size(); ++i) {
+        circle& c = m_circles.at(i);
 
-    if (m_x - m_radius < 0) {
-        m_x = m_radius + 0.1;
-        m_x_speed = -m_x_speed;
-    }
-    else if (m_width < m_x + m_radius) {
-        m_x = m_width - m_radius;
-        m_x_speed = -m_x_speed;
-    }
+        c.m_x += deltaT * c.m_x_speed;
+        c.m_y += deltaT * c.m_y_speed;
 
-    if (m_y - m_radius < 0) {
-        m_y = m_radius + 0.1;
-        m_y_speed = -m_y_speed;
-    }
-    else if (m_length < m_y + m_radius) {
-        m_y = m_length - m_radius - 0.1;
-        m_y_speed = -m_y_speed;
+        if (c.m_x - c.m_radius < 0) {
+            c.m_x = c.m_radius + 0.1;
+            c.m_x_speed = -c.m_x_speed;
+        }
+        else if (m_width < c.m_x + c.m_radius) {
+            c.m_x = m_width - c.m_radius;
+            c.m_x_speed = -c.m_x_speed;
+        }
+
+        if (c.m_y - c.m_radius < 0) {
+            c.m_y = c.m_radius + 0.1;
+            c.m_y_speed = -c.m_y_speed;
+        }
+        else if (m_length < c.m_y + c.m_radius) {
+            c.m_y = m_length - c.m_radius - 0.1;
+            c.m_y_speed = -c.m_y_speed;
+        }
     }
 }
 
 void draw_cirle(float x, float y, float radius, float step = 1)
 {
-    glBegin(GL_TRIANGLE_FAN);
-        glVertex2f(x, y);
-        for(float angle = 0; angle < 360; angle += step) {
-            glVertex2f(x + std::sin(angle) * radius, y + std::cos(angle) * radius);
-        }
-    glEnd();
+    glVertex2f(x, y);
+    for(float angle = 0; angle < 360; angle += step) {
+        glVertex2f(x + std::sin(angle) * radius, y + std::cos(angle) * radius);
+    }
 }
 
 void TwoDRectangleBox::draw()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1, 0, 0);
+    glColor3f(0.2, 0.2, 0.2);
 
-    draw_cirle(m_x, m_y, m_radius);
+    for (unsigned int i = 0; i < m_circles.size(); ++i) {
+        const circle& c = m_circles.at(i);
+        glBegin(GL_TRIANGLE_FAN);
+        glColor3f(c.m_color_r, c.m_color_g, c.m_color_b);
+        draw_cirle(c.m_x, c.m_y, c.m_radius);
+        glEnd();
+    }
+}
+
+void TwoDRectangleBox::initialize()
+{
+    for (unsigned int i = 0; i < 10; ++i) {
+        circle c;
+        c.m_radius = (std::min(m_width, m_length)/30),
+        c.m_x = rand() % int(m_width - c.m_radius);
+        if (c.m_x < c.m_radius) c.m_x = c.m_radius;
+        c.m_y = rand() % int(m_length - c.m_radius);
+        if (c.m_y < c.m_radius) c.m_y = c.m_radius;
+
+        c.m_x_speed = 36 + rand() % 50;
+        c.m_y_speed = 58 + rand() % 62;
+
+        const int denominator = 100;
+
+        c.m_color_r = float(rand() % denominator ) / float(denominator);
+        c.m_color_g = float(rand() % denominator ) / float(denominator);
+        c.m_color_b = float(rand() % denominator ) / float(denominator);
+
+        m_circles.push_back(c);
+    }
 }
--- apps/circles_moving_in_container/TwoDRectangleBox.h
+++ apps/circles_moving_in_container/TwoDRectangleBox.h
@@ -1,6 +1,8 @@
 #ifndef TWODRECTANGLEBOX_H
 #define TWODRECTANGLEBOX_H
 
+#include <vector>
+
 class TwoDRectangleBox
 {
 public:
@@ -14,13 +16,21 @@ public:
     void draw();
 
 private:
+	struct circle
+	{
+		float m_x;
+		float m_y;
+		float m_radius;
+		float m_x_speed;
+		float m_y_speed;
+        float m_color_r;
+        float m_color_g;
+        float m_color_b;
+	};
+
     double m_width;
     double m_length;
-    float m_x;
-    float m_y;
-    float m_radius;
-    float m_x_speed;
-    float m_y_speed;
+    std::vector<circle> m_circles;
 };
 
 #endif // TWODRECTANGLEBOX_H
